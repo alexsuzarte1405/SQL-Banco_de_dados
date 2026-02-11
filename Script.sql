2a12511f71e47ab1721aee3e451ca3b9ff6b1da7
alexsuzarte1405 <alexsandrosuzarte@gmail.com>
2026-02-10 23:35:39 -0300

    Add files via upload

--- Script.sql
+++ Script.sql
@@ -1,27 +1,26 @@
-create database supermercado;
- 
-create table cliente (
-id int not null,
-nome varchar(50) not null,
-cpf varchar(11) not null,
-constraint pk_id_cliente primary key (id)
-)
- 
-create table produto(
-id int not null,
-descricao varchar(100) not null,
-cod_barras varchar(14),
-valor decimal(16,2)
-)
- 
-alter table produto add constraint pk_id_produto primary key (id)
+-- Criação da tabela de clientes
+CREATE TABLE cliente (
+    id INT NOT NULL,
+    nome VARCHAR(50) NOT NULL,
+    cpf VARCHAR(11) NOT NULL,
+    CONSTRAINT pk_id_cliente PRIMARY KEY (id)
+);
 
-alter table produto add column situacao boolean not null
- 
-alter table produto alter column descricao type varchar(200)
- 
-alter table produto drop column situacao
- 
+-- Criação da tabela de produtos
+CREATE TABLE produto (
+    id INT NOT NULL,
+    descricao VARCHAR(100) NOT NULL,
+    cod_barras VARCHAR(14),
+    valor DECIMAL(16,2)
+);
 
+-- Adicionando a chave primária em produto
+ALTER TABLE produto ADD CONSTRAINT pk_id_produto PRIMARY KEY(id);
+
+-- Adicionando a coluna de situação (status)
+ALTER TABLE produto ADD COLUMN situacao BOOLEAN NOT NULL;
+
+-- Aumentando o limite de caracteres da descrição
+ALTER TABLE produto ALTER COLUMN descricao TYPE VARCHAR(200);
 
  
