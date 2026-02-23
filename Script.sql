aabfa49f5cde07db39556bd822b88afffb90d2ed
alexsuzarte1405 <alexsandrosuzarte@gmail.com>
2026-02-22 22:05:59 -0300

    Update Script.sql

--- Script.sql
+++ Script.sql
@@ -1,26 +1,56 @@
--- Criação da tabela de clientes
-CREATE TABLE cliente (
-    id INT NOT NULL,
-    nome VARCHAR(50) NOT NULL,
-    cpf VARCHAR(11) NOT NULL,
-    CONSTRAINT pk_id_cliente PRIMARY KEY (id)
-);
-
--- Criação da tabela de produtos
-CREATE TABLE produto (
-    id INT NOT NULL,
-    descricao VARCHAR(100) NOT NULL,
-    cod_barras VARCHAR(14),
-    valor DECIMAL(16,2)
-);
-
--- Adicionando a chave primária em produto
-ALTER TABLE produto ADD CONSTRAINT pk_id_produto PRIMARY KEY(id);
-
--- Adicionando a coluna de situação (status)
-ALTER TABLE produto ADD COLUMN situacao BOOLEAN NOT NULL;
-
--- Aumentando o limite de caracteres da descrição
-ALTER TABLE produto ALTER COLUMN descricao TYPE VARCHAR(200);
-
+- create database supermercado;
  
+--create table cliente (
+--id int not null,
+--nome varchar(50) not null,
+--cpf varchar(11) not null,
+--constraint pk_id_cliente primary key (id)
+--)
+ 
+--create table produto(
+--id int not null,
+--descricao varchar(100) not null,
+--cod_barras varchar(14),
+--valor decimal(16,2)
+--)
+ 
+--alter table produto add constraint pk_id_produto primary key (id)
+ 
+--alter table produto add column situacao boolean not null
+ 
+--alter table produto alter column descricao type varchar(200)
+ 
+
+insert into produto (id, descricao, cod_barras, valor, situacao)
+values (2, 'cafe', '8721398123', 10.20, true )
+
+insert into cliente (id, nome, cpf)
+values (4, 'Zeca', '00100000000')
+
+update cliente set nome = 'ax' where ID = 2
+
+delete from produto where id = 1 or id = 4 or id = 6
+ 
+select * from produto 
+
+select 
+    *
+from     
+    produto 
+where 
+    id = 1 
+    
+select
+	id, descricao
+from
+	produto
+order by id desc    
+    
+delete from produto;
+insert into produto (id, descricao, cod_barras, valor, situacao )
+values (5, 'Arroz', '8721398123', 10.20, true);
+insert into produto (id, descricao, cod_barras, valor, situacao )
+values (2, 'Cafe', '8721398123', 10.20, true);
+insert into produto (id, descricao, cod_barras, valor, situacao )
+values (3, 'Macarrao', '8721398123', 10.20, true);    
+    
